85369ab16cf0d46a27f41c2b1a1fc7d154f5839b
betosilvaz <gilbertozn527@gmail.com>
2025-08-05 00:58:06 -0300

    remove scrolling feature if browser detects touch support

--- js/scroll_movement.js
+++ js/scroll_movement.js
@@ -2,14 +2,19 @@
 
 let sections = document.querySelectorAll("[data-section]");
 let currentSection = 0;
-
-document.body.addEventListener("wheel", function(e) {
+let action = function (e) {
     let direction = (e.deltaY > 0) ? "bottom" : "top";
-    if(direction == "bottom" && currentSection < sections.length - 1) {
+    if (direction == "bottom" && currentSection < sections.length - 1) {
         currentSection++;
-    } else if(direction == "top" && currentSection > 0) {
+    } else if (direction == "top" && currentSection > 0) {
         currentSection--;
     }
     let nextElement = sections[currentSection];
     nextElement.scrollIntoView(true);
+};
+
+document.body.addEventListener("wheel", action);
+
+document.body.addEventListener("touchstart", function(e) {
+    document.body.removeEventListener("wheel", action);
 });
